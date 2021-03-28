f2b98aa0c594b25b79dd5b77a3593b7f38c4ecf0
Dorian <dorek7@vp.pl>
2021-03-28 13:42:28 +0200

    Pushed uncommitted

---
+++ src/main/java/com/dorek98/SeriesShelfApplication.java
@@ -0,0 +1,13 @@
+package com.dorek98;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dorek98/SeriesShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorek98;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
